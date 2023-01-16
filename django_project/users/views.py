856759787769d40e922e4cda8eac4b5805b25bfd
Mayank <mayankxsharmaone@gmail.com>
2023-01-17 01:18:00 +0530

    New changes till video 9

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserReisterForm 
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserReisterForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
-        form = UserReisterForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+@login_required
+def profile(request):
+        u_form = UserUpdateForm()
+        p_form = ProfileUpdateForm()
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+        
+        
+        context = {
+            'u_form': u_form,
+            'p_form': p_form
+            }
+
+        return render(request, 'users/profile.html', context)
